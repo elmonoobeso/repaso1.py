c75ae555a981a43ae32a078551b60a82e1beafd5
elmonoobeso <miguelangelleo45@gmail.com>
2026-08-11 13:48:31 +0200

    repas 1 de archivos

---
+++ repas12.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+import csv
+dataset=Path(r"C:\Users\migue\Downloads\repaso1.py\dataset")
+ruta=dataset/"inventario_diario.csv"
+class CSVAnalyzer:
+    def __init__(self,ruta):
+        self.__file_path=ruta
+    def get_valid_stocks(self):
+        with open(self.__file_path,"r",newline='',encoding="utf-8") as inventario:
+             reader=csv.reader(inventario)
+             columna=next(reader,None)
+             validos=[]
+             for linea in reader:
+                if row:
+                     validos.append(row)
+             return
+
+            
+        
+    def parseo(self):
+        total=lambda stock,precio:stock*precio
+        with open(self.__file_path,"r",newline='',encoding="utf-8") as inventario:
+                reader=csv.reader(inventario)
+                next(reader,None)
+                resultado=[]
+                for line in reader:
+                     try:
+                          nombre,stock,precio=line
+                          stock_int=int(stock)
+                          precio_flo=float(precio)
+                          calculo=total(stock_int,precio_flo)
+                          print(f"Producto: {nombre}, total:{calculo}")
+                          
+                     except Exception as error:
+                          print(error)
+                return resultado
+
+prueba=CSVAnalyzer(ruta)
+prueba.parseo()
+        
+                        
+
+                          
+        
