85eadee0d69f483d333259b59fe422a34ccb81ff
elmonoobeso <miguelangelleo45@gmail.com>
2026-08-20 18:13:53 +0200

    e

---
+++ repas13.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+prueba_json=Path(r"C:\Users\migue\Downloads\repaso1.py\dataset\doc_prueba.json")
+ruta=dataset/"doc_prueba.json"
+class JSONAPIAnalyzer:
+    def __init__(self,ruta):
+        self.__file_path=ruta
+    def get_verified_amounts(self):
+        with open(self.__file_path,)
+
+        
+with open (ruta,)
+
